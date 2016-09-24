55462a0843d3a34dca13c8733bb91424818d563d
Ben Ward <bw@ben-ward.net>
2016-09-24 23:16:23 +0100

    Added views for owner, location and board game details

--- src/GctgsWeb/Controllers/BoardGamesController.cs
+++ src/GctgsWeb/Controllers/BoardGamesController.cs
@@ -5,6 +5,7 @@ using GctgsWeb.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.EntityFrameworkCore;
 
 namespace GctgsWeb.Controllers
 {
@@ -22,7 +23,41 @@ namespace GctgsWeb.Controllers
 
         public IActionResult Index()
         {
-            return View(_context.BoardGames.ToList());
+            return View(_context.BoardGames
+                .Include(boardGame => boardGame.Owner)
+                .OrderBy(boardGame => boardGame.Name)
+                .ToList());
+        }
+
+        [HttpGet("boardgames/boardgame/{id}")]
+        public IActionResult BoardGame(int id)
+        {
+            var result = _context.BoardGames
+                .Where(boardGame => boardGame.Id == id)
+                .Include(boardGame => boardGame.Owner)
+                .SingleOrDefault();
+
+            return result != null ? View(result) : (ActionResult) new NotFoundResult();
+        }
+
+        [HttpGet("boardgames/locations/{location}")]
+        public IActionResult Locations(string location)
+        {
+            return View("Index", _context.BoardGames
+                .Include(boardGame => boardGame.Owner)
+                .Where(boardGame => boardGame.Location == location)
+                .OrderBy(boardGame => boardGame.Name)
+                .ToList());
+        }
+
+        [HttpGet("boardgames/owners/{owner}")]
+        public IActionResult Owners(string owner)
+        {
+            return View("Index", _context.BoardGames
+                .Include(boardGame => boardGame.Owner)
+                .Where(boardGame => boardGame.Owner.Crsid == owner)
+                .OrderBy(boardGame => boardGame.Name)
+                .ToList());
         }
 
         public IActionResult Create()
